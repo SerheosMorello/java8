1e481c0379b89a40eef501a6b6a2e29ebcf5552c
Sergii.Pankrashyn <Sergii.Pankrashyn.9@fntsoftware.com>
2020-02-17 18:19:50 +0200

    add Enum operations

--- src/enums/App.java
+++ src/enums/App.java
@@ -2,9 +2,17 @@ package enums;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println(Buttons.YES.getRussian());
-        System.out.println(Buttons.NO.getRussian());
-        System.out.println(Buttons.YES.getEnglish());
-        System.out.println(Buttons.NO.getEnglish());
+        System.out.println(Buttons.YES.getRussian()); // Да
+        System.out.println(Buttons.NO.getRussian()); // Нет
+        System.out.println(Buttons.YES.getEnglish()); // Yes
+        System.out.println(Buttons.NO.getEnglish()); // No
+
+        Operation sum = Operation.SUM;
+        System.out.println(sum.action(10, 4));   // 14
+        Operation multiplay = Operation.MULTIPLY;
+        System.out.println(multiplay.action(6, 4));    // 24
+        Operation subtract = Operation.SUBTRACT;
+        System.out.println(subtract.action(6, 4));    // 2
+
     }
 }

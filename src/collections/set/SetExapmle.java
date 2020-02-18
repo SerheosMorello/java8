39436e54cf514ff35e13ca9b0fae71d80214e067
Sergii.Pankrashyn <Sergii.Pankrashyn.9@fntsoftware.com>
2020-02-18 16:29:21 +0200

    add HashSet example

---
+++ src/collections/set/SetExapmle.java
@@ -0,0 +1,50 @@
+package collections.set;
+
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+public class SetExapmle {
+    public static void main(String[] args) {
+
+        // init Sets
+        HashSet<Integer> setList = new HashSet<>(Arrays.asList(1, 2, 3, 4));
+        HashSet<Integer> setList2 = new HashSet<>();
+
+        // show size of Set
+        System.out.println(setList.size()); // 4
+        System.out.println(setList2.size()); // 0
+
+        // show Sets
+        System.out.println(setList); // [1, 2, 3, 4]
+        System.out.println(setList2); // []
+
+        // Set is Empty
+        System.out.println("setList isEmpty = " + setList.isEmpty()); // false
+        System.out.println("setList2 isEmpty = " + setList2.isEmpty()); // true
+
+        // add value to Set
+        setList2.add(5);
+        System.out.println(setList2); // [5]
+
+        // add Set to Set
+        setList2.addAll(setList);
+        System.out.println(setList2); // [1, 2, 3, 4, 5]
+
+        // delete Set from Set
+        setList2.removeAll(setList); // [5]
+
+        // delete value from Set
+        setList2.remove(5);
+        System.out.println(setList2); // []
+
+        // clear Set
+        setList2.addAll(setList);
+        System.out.println(setList2); // [1, 2, 3, 4,]
+        setList2.clear();
+        System.out.println(setList2); // []
+
+        // Set contains value
+        System.out.println("Set contains value =" + setList.contains(1)); // true
+        System.out.println("Set contains value =" + setList.contains(5)); // false
+    }
+}
